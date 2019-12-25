e4d84fc374da6cc1224bf2e74810e9860369677c
Locas.hu <hmrcode@gmail.com>
2019-12-25 14:03:38 +0800

    update pom

--- src/main/java/com/cloudwise/project/init/BaseJob.java
+++ src/main/java/com/cloudwise/project/init/BaseJob.java
@@ -1,17 +1,17 @@
-package com.cloudwise.project.init;
-
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-
-public abstract class BaseJob implements Job {
-	
-	public abstract void collectorData();
-
-	@Override
-	public void execute(JobExecutionContext context) throws JobExecutionException {
-		
-		collectorData();
-	}
-
-}
+//package com.cloudwise.project.init;
+//
+//import org.quartz.Job;
+//import org.quartz.JobExecutionContext;
+//import org.quartz.JobExecutionException;
+//
+//public abstract class BaseJob implements Job {
+//
+//	public abstract void collectorData();
+//
+//	@Override
+//	public void execute(JobExecutionContext context) throws JobExecutionException {
+//
+//		collectorData();
+//	}
+//
+//}
--- src/main/java/com/cloudwise/project/init/SchedulerRunner.java
+++ src/main/java/com/cloudwise/project/init/SchedulerRunner.java
@@ -1,58 +1,58 @@
-package com.cloudwise.project.init;
-
-import com.cloudwise.project.init.clean.DeleteFile;
-import lombok.extern.slf4j.Slf4j;
-import org.reflections.Reflections;
-import org.reflections.scanners.MethodAnnotationsScanner;
-import org.reflections.scanners.SubTypesScanner;
-import org.reflections.scanners.TypeAnnotationsScanner;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
-import org.reflections.util.FilterBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-
-import java.util.Set;
-
-@Component
-@Order(value = 1)
-@Slf4j
-public class SchedulerRunner implements CommandLineRunner {
-
-	@Autowired
-	private BauschScheduler scheduler;
-
-	private void schedeleTask() {
-
-//		// 采集任务
-//		String collectorpackage2 = "com.cloudwise.project.init.clean";
-//		// 目的是实例化com.cloudwise.project.init.clean路径下的所有类（通过反射）
-//		ConfigurationBuilder configBuilder2 = new ConfigurationBuilder()
-//				.filterInputsBy(new FilterBuilder().includePackage(collectorpackage2))
-//				.setUrls(ClasspathHelper.forPackage(collectorpackage2))
-//				.setScanners(new TypeAnnotationsScanner(), new MethodAnnotationsScanner(), new SubTypesScanner(false));
-//		Reflections reflections2 = new Reflections(configBuilder2);
-//		Set<Class<? extends BaseJob>> clazz2 = reflections2.getSubTypesOf(BaseJob.class);
-//		for (Class<? extends BaseJob> clazz : clazz2) {
+//package com.cloudwise.project.init;
 //
-//			scheduler.collectDataByInterval(clazz, 1);
+//import com.cloudwise.project.init.clean.DeleteFile;
+//import lombok.extern.slf4j.Slf4j;
+//import org.reflections.Reflections;
+//import org.reflections.scanners.MethodAnnotationsScanner;
+//import org.reflections.scanners.SubTypesScanner;
+//import org.reflections.scanners.TypeAnnotationsScanner;
+//import org.reflections.util.ClasspathHelper;
+//import org.reflections.util.ConfigurationBuilder;
+//import org.reflections.util.FilterBuilder;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.stereotype.Component;
 //
+//import java.util.Set;
+//
+//@Component
+//@Order(value = 1)
+//@Slf4j
+//public class SchedulerRunner implements CommandLineRunner {
+//
+//	@Autowired
+//	private BauschScheduler scheduler;
+//
+//	private void schedeleTask() {
+//
+////		// 采集任务
+////		String collectorpackage2 = "com.cloudwise.project.init.clean";
+////		// 目的是实例化com.cloudwise.project.init.clean路径下的所有类（通过反射）
+////		ConfigurationBuilder configBuilder2 = new ConfigurationBuilder()
+////				.filterInputsBy(new FilterBuilder().includePackage(collectorpackage2))
+////				.setUrls(ClasspathHelper.forPackage(collectorpackage2))
+////				.setScanners(new TypeAnnotationsScanner(), new MethodAnnotationsScanner(), new SubTypesScanner(false));
+////		Reflections reflections2 = new Reflections(configBuilder2);
+////		Set<Class<? extends BaseJob>> clazz2 = reflections2.getSubTypesOf(BaseJob.class);
+////		for (Class<? extends BaseJob> clazz : clazz2) {
+////
+////			scheduler.collectDataByInterval(clazz, 1);
+////
+////		}
+//		//定时删除数据
+//		scheduler.startJobByCron(DeleteFile.class, "0 0 0 1/15 * ? ");
+//
+//	}
+//
+//	@Override
+//	public void run(String... paramVarArgs) throws Exception {
+//
+//		try {
+//			scheduler.start();// 开启调度器
+//			schedeleTask();
+//		} catch (Exception e) {
+//			log.error("start scheduler error", e);
 //		}
-		//定时删除数据
-		scheduler.startJobByCron(DeleteFile.class, "0 0 0 1/15 * ? ");
-		
-	}
-
-	@Override
-	public void run(String... paramVarArgs) throws Exception {
-
-		try {
-			scheduler.start();// 开启调度器
-			schedeleTask();
-		} catch (Exception e) {
-			log.error("start scheduler error", e);
-		}
-	}
-}
+//	}
+//}
